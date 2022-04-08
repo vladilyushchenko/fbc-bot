623838072f99a315bc2d97d46cca00833e5965a5
vladilyushchenko <vladislavilyushchenko@gmail.com>
2022-04-08 13:45:50 +0300

    Initial commit

---
+++ src/main/java/com/fbc/bot/FbcBotApplication.java
@@ -0,0 +1,13 @@
+package com.fbc.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbcBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbcBotApplication.class, args);
+	}
+
+}
